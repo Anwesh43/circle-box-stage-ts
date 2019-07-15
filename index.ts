ca166a96f4a0e3653841032adcf278e00156e6d1
anwesh43 <anweshthecool0@gmail.com>
2019-07-15 15:53:56 +0530

    adding ScaleUtil class

--- index.ts
+++ index.ts
@@ -8,6 +8,30 @@ const nodes : number = 5
 const foreColor : string = "#"
 const backColor : string = "#BDBDBD"
 
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, b : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, maxScale(scale, i, n)) * n
+    }
+
+    static scaleFactor(scale : number) : number {
+        return Math.floor(scale / scDiv)
+    }
+
+    static mirrorValue(scale : number, a : number, b : number) : number {
+        const k : number = ScaleUtil.scaleFactor(scale)
+        return (1 - k) / a + k / b
+    }
+
+    static updateValue(scale : number, dir : number, a : number, b : number) : number {
+        return ScaleUtil.mirrorValue(scale, a, b) * dir * scGap
+    }
+}
+
 class CircleBoxStage {
 
     canvas : HTMLCanvasElement = document.createElement('canvas')
